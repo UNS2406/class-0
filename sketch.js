6188482229b7967bbb25a5d3181d5c61db12e692
UNS2406 <71777971+UNS2406@users.noreply.github.com>
2023-05-03 16:41:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//Class 0
+
+//Goal: To create the basic structure of the PONG game
+
+//1. Ground Outline
+function setup() {
+  //createCanvas(width, height);
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("green");
+  
+  text( "x:" +mouseX + ", y:" + mouseY, mouseX, mouseY);
+  
+  //to draw a rectangle or square we use:
+  //rect(x, y, width, height);
+
+  //2. Square Ball
+
+  //3. LEFT PADDLE
+
+  //4. RIGHT PADDLE
+  
+  
+}
+
+
